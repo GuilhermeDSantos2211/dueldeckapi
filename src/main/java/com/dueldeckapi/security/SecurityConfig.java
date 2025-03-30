361ca4a8a3f567225dbc577e5708ca8f4caf6f4f
Guilherme <153827669+GuilhermeDSantos2211@users.noreply.github.com>
2025-03-30 17:58:01 -0300

    Update SecurityConfig.java

--- src/main/java/com/dueldeckapi/security/SecurityConfig.java
+++ src/main/java/com/dueldeckapi/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll() // Endpoints públicos para autenticação
+                        .requestMatchers("/api/users/**").permitAll() // Endpoints públicos para autenticação
                         .anyRequest().authenticated()
                 );
 
